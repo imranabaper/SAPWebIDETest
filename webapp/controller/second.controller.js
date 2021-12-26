f5b30c684d7b220dd89c4728f979e023b3cf5bc8
imranabaper <imranabaper@gmail.com>
2021-12-26 18:12:27 +0530

    UdemyProject

---
+++ webapp/controller/second.controller.js
@@ -0,0 +1,30 @@
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History", "sap/m/MessageToast"], function(Controller, History,
+	MessageToast) {
+	"use strict";
+	return Controller.extend("satya.prasad.mvcapp.controller.second", {
+		onInit: function() {
+			this._oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this._oRouter.getRoute("detail").attachPatternMatched(this._oRoutePatternMatched, this);
+		},
+		_oRoutePatternMatched: function(oEvent) {
+			var sPath = "/SweetsSupplier/" + oEvent.getParameter("arguments").ID;
+			this.getView().bindElement(sPath);
+		},
+		onNavPress: function() {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				this._oRouter.navTo("master");
+			}
+
+		},
+		onPress: function() {
+			var msg = this.getView().getModel("i18n").getResourceBundle().getText("Name", ['Hai How are you?']);
+			MessageToast.show(msg);
+		}
+	});
+
+});
